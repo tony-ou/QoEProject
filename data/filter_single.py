94377a32f1db9ff438ac9583d8e9c18a28122376
Yiyang Ou <yiyangou@slowpoke.cs.uchicago.edu>
2020-01-27 18:31:54 -0600

    add & tested filter script

--- data/filter_single.py
+++ data/filter_single.py
@@ -4,6 +4,39 @@ import os
 import sys
 import subprocess
 
+
+#Sample filter function
+def filter_single_video(lengths, video_times, rating_times, video_order, scores):
+    #First check if user watching alines with video length
+    #0.5 sec tolerance for black screen at the end
+    for i in range(len(lengths)):
+        if (lengths[i] - 0.5> video_times[i]):
+            return True
+
+    #Then check if key moment has least score 
+    #suppose key moment is video 0
+    if min(scores) > scores[0]:
+        return True
+
+    #Then check if original video (#1) has highest score 
+    if max(scores) < scores[1]:
+        return True
+    
+    #Then check if key moment is lower than orig vid
+    if scores[0] >= scores[1]:
+        return True
+    
+    return False #We don't move this user to rejected folder
+
+#Parse data from user result file 
+def parse_results(lines):
+    video_times = list(map(int,lines[2].strip().split(','))) #read times spent on each video  
+    rating_times = list(map(int,lines[3].strip().split(','))) #read times spent on each rating  
+    video_order = list(map(int,lines[1].strip().split(','))) #read the video order seen by the surveyee
+    scores = list(map(int,lines[0].strip().split(','))) #read scores
+
+    return video_times, rating_times, video_order, scores
+
 #get video length
 def getLength(filename):
     result = subprocess.Popen(["ffprobe", filename], universal_newlines=True, 
@@ -21,37 +54,28 @@ def getLength(filename):
     return tot
 
 #input from the cmd line script
-result_path = sys.argv[-1]
-reject_path = sys.argv[-2]
-vid_path = sys.argv[-3]
+result_path, reject_path, vid_path = sys.argv[-1], sys.argv[-2], sys.argv[-3]
 
 list_dir = os.listdir(vid_path)
-count = 0
-lengths = []
+lengths = [] #actual video lengths
 for vid in list_dir:
     if vid.endswith(".mp4"):
-        count += 1
         full_vid_path = vid_path + "/" + vid
-        lengths.append(getLength(full_vid_path))
+        lengths.append(getLength(full_vid_path)) 
 
 move = False
 result_files = os.listdir(result_path)
 
+
 for result_file in result_files:
+    print(result_file)
     #filter a single result
     result = result_path + "/" + result_file
     with open(result, "r") as fp:
         lines = fp.readlines()
         move = False
 
-        video_times = list(map(int,lines[2].strip().split(','))) #read times spent on each video  
-        rating_times = list(map(int,lines[3].strip().split(','))) #read times spent on each rating  
-        video_order = list(map(int,lines[1].strip().split(','))) #read the video order seen by the surveyee
-        scores = list(map(int,lines[0].strip().split(','))) #read scores
-    
-        #print(video_times,rating_times,video_order,scores)
-
-        #TODO: insert filtering logic and set move accordingly
-
+        #insert customized filter here 
+        move = filter_single_video(lengths,*parse_results(lines))
         if move:
             os.system("mv {} ../rejected_results".format(result))
